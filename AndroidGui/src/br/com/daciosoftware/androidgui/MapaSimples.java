068387ef710a28582339cc4b0c61ff275430eb61
fdacio <fdacio@fdacio-14I>
2015-05-27 21:54:57 -0300

    Correcao do maps

--- AndroidGui/src/br/com/daciosoftware/androidgui/MapaSimples.java
+++ AndroidGui/src/br/com/daciosoftware/androidgui/MapaSimples.java
@@ -4,10 +4,11 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
